09feb5120491d4c132af99ada57c319b0855c03b
Kyle Rockman <kyle.rockman@mac.com>
2014-05-20 08:47:42 -0500

    removing debug print that didn't work

--- ohrockman/settings.py
+++ ohrockman/settings.py
@@ -85,7 +85,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-print STATIC_ROOT
 STATIC_URL = 'http://media.ohrockman.com/static/'
 
 TEMPLATE_DIRS = (
