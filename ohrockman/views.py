50cc7dc54c8b712113e5d2e378e0a12d974cb42c
Kyle Rockman <kyle.rockman@mac.com>
2014-05-24 20:16:50 -0500

    need to pass in oscar and helen family member objects to the index template

--- ohrockman/views.py
+++ ohrockman/views.py
@@ -1,7 +1,13 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
+from familytree.models import FamilyMember
+
+
 def index(request):
-    return render(request, 'index.html', {})
+    oscar = get_object_or_404(FamilyMember, pk=1)
+    helen = get_object_or_404(FamilyMember, pk=13)    
+    return render(request, 'index.html', {'oscar':oscar, 'helen':helen})
 
 def maintainence(request):
     return render(request, 'maintainence.html', {})
