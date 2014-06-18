0f33d8bba1f46b743edb1e9c15ac2933387214f9
Kyle Rockman <kyle.rockman@mac.com>
2014-06-17 19:47:23 -0500

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@ echo $WORKSPACE
 export PROJECT_NAME=ohrockman
 export VENV_ROOT=/Users/zoeserver/.virtualenvs/${PROJECT_NAME}/
 export WEB_ROOT=/Volumes/STORAGE/Web/${PROJECT_NAME}
+
+rm -rvf ${WEB_ROOT}
+mkdir ${WEB_ROOT}
+cp -rv ./* ${WEB_ROOT}
+cd ${WEB_ROOT}
+
 ln -s ${VENV_ROOT} ./.virtualenv
 . ./.virtualenv/bin/activate
 pip install --upgrade -r ./requirements.txt
 rm -f /usr/local/etc/uwsgi/apps/${PROJECT_NAME}.ini
 mv ./wsgi.ini /usr/local/etc/uwsgi/apps/${PROJECT_NAME}.ini
 
-rm -rvf ${WEB_ROOT}
-mkdir ${WEB_ROOT}
-cp -rv ./* ${WEB_ROOT}
-cd ${WEB_ROOT}
 python manage.py collectstatic --noinput
