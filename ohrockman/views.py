7ee6031af117451c792cc56c7f5e7ede89118690
Kyle Rockman <kyle.rockman@mac.com>
2014-06-24 07:46:50 -0500

    prep work for family tree app

--- ohrockman/views.py
+++ ohrockman/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render_to_response, render
 from django.shortcuts import render
 
-from familytree.models import FamilyMember
+from familytree.models import FamilyMember, Marriage
 
 
 def index(request):
@@ -11,3 +12,19 @@ def index(request):
 
 def maintainence(request):
     return render(request, 'maintainence.html', {})
+
+def family_member(request, member_id):
+    person = get_object_or_404(FamilyMember, pk=member_id)
+    marriages = Marriage.objects.marriage_with(person)
+    return render_to_response('member_info.html', {'person':person, 'marriages':marriages})
+
+def family_member_search(request):
+    first = request.GET.get('first',None)
+    middle = request.GET.get('middle',None)
+    last = request.GET.get('last',None)
+    maiden = request.GET.get('maiden',None)
+    person_list = FamilyMember.objects.search_members(first, middle, last, maiden)
+    if not person_list:
+        raise Http404
+        #return HttpResponse('Search Fields were %s - %s - %s - %s'%(first_name, middle_name, last_name, maiden_name)) 
+    return render_to_response('member_list.html', {'person_list':person_list})
