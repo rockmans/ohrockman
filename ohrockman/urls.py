caec75c458672dd1ffaaccae96efde1e8ce2c1e0
Kyle Rockman <kyle.rockman@mac.com>
2014-07-03 16:09:44 -0500

    trying to fix the URLS

--- ohrockman/urls.py
+++ ohrockman/urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import patterns, include, url, handler404, handler500
+from django.conf.urls.static import static
+
+
 from django.contrib import admin
 admin.autodiscover()
+
 
 from ohrockman import views
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', views.index, name='index'),
-    #url(r'^$', views.maintainence, name='maintainence'),
-    url(r'^familytree$', views.family_member, {'member_id':1}),
-    url(r'^familytree/(?P<member_id>\d+)/$', views.family_member),
-    
-    
-    url(r'^familytree/search', views.family_member_search),
-    url(r'^reunion/$', views.reunion),
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', views.index, name='index'),
+                       #url(r'^$', views.maintainence, name='maintainence'),
+                       url(r'^familytree$', views.family_member, {'member_id':1}),
+                       url(r'^familytree/(?P<member_id>\d+)/$', views.family_member),
+                       
+                       
+                       url(r'^familytree/search', views.family_member_search),
+                       url(r'^reunion/$', views.reunion),
+                       url(r'^admin/', include(admin.site.urls)),
 )
