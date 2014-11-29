9886dcd82c73bcc3431a02a0b41282f7e79950fe
Kyle Rockman <kyle.rockman@mac.com>
2014-11-29 17:13:13 -0600

    new deploy structure
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

---
+++ jenkins/deploy.sh
@@ -0,0 +1,41 @@
+echo $WORKSPACE
+export PROJECT_NAME=ohrockman
+export APP_VERSION=`git rev-parse HEAD | cut -c -12`
+export VENV_VERSION=`sha1sum ./requirements.txt | cut -c -12`
+export ROOT="/usr/local/$PROJECT_NAME"
+export APP_DIR="$ROOT/app_versions/$APP_VERSION"
+export VENV_DIR="$ROOT/venv_versions/$VENV_VERSION"
+export TEST_APP_DIR="$ROOT/app_test"
+export ACTIVE_APP_DIR="$ROOT/app_$APP_ENV"
+export ACTIVE_VENV_DIR="$ROOT/app_venv_$APP_ENV"
+export MEDIA_DIR="$ROOT/media"
+export DB_DIR="$ROOT/db_$APP_ENV"
+
+mkdir -p "$MEDIA_DIR"
+mkdir -p "$DB_DIR"
+
+if [[ ! -e "$APP_DIR" ]]
+then
+  mkdir -p "$APP_DIR"
+  cp -rv "./." "$APP_DIR"
+fi
+
+if [[ ! -e "$VENV_DIR/bin/activate" ]]
+then
+  virtualenv "$VENV_DIR"
+  . "$VENV_DIR/bin/activate"
+  pip install -r "./requirements.txt"
+else
+  . "$VENV_DIR/bin/activate"
+fi
+
+ln -snf "$APP_DIR" "$TEST_APP_DIR"
+cd "$TEST_APP_DIR"
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+
+ln -snf "$APP_DIR" "$ACTIVE_APP_DIR"
+ln -snf "$VENV_DIR" "$ACTIVE_VENV_DIR"
+
+cp -f "$WORKSPACE/jenkins/nginx.conf" "/etc/nginx/sites-enabled/$PROJECT_NAME.conf"
+cp -f "$WORKSPACE/jenkins/uwsgi_$APP_ENV.ini" "/usr/local/uwsgi/confs/${PROJECT_NAME}_$APP_ENV.ini"
--- ohrockman/settings.py
+++ ohrockman/settings.py
@@ -7,33 +7,56 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
+from photologue import PHOTOLOGUE_APP_DIR
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.join(BASE_DIR, 'ohrockman')
-DB_ROOT = '/www/database/ohrockman'
+APP_ENV = os.environ.get('APP_ENV', 'local')
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+REMOTE_ROOT = '/usr/local/kyleandemily'
 
-DEBUG = True
-TEMPLATE_DEBUG = False
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
+if APP_ENV == "local":
+    DB_ROOT = PROJECT_DIR
+else:
+    DB_ROOT = os.path.join(REMOTE_ROOT, 'db_' + APP_ENV)
 
-with open(os.path.join(BASE_DIR, 'key.txt')) as f:
+# SECURITY WARNING: keep the secret key used in production secret!
+with open(os.path.join(PROJECT_DIR, 'key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 
-ALLOWED_HOSTS = ['www.ohrockman.com',
-                 'ohrockman.com',
-                 'localhost',
-                 'localhost:9091']
+# SECURITY WARNING: don't run with debug turned on in production!
+if APP_ENV != "prod":
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = [
+    'www.ohrockman.com',
+    'ohrockman.com',
+    'localhost',
+]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DB_ROOT, 'family.db'),
+        'NAME': os.path.join(DB_ROOT, 'default.db'),
+    },
+    'admin': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DB_ROOT, 'admin.db'),
+    },
+    'familytree': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DB_ROOT, 'familytree.db'),
+    },
+    'photos': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DB_ROOT, 'photo.db'),
     }
 }
-
+# DATABASE_ROUTERS = [
+#     'kyleandemily.wedding.db_router.PhotologueRouter',
+# ]
 
 # Application definition
 
@@ -44,6 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat',
     'photologue',
     'south',
     'sortedm2m',    
@@ -58,6 +82,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'ohrockman.urls'
@@ -83,18 +108,31 @@ SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'http://media.ohrockman.com/'
+if APP_ENV == "local":
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+    STATIC_URL = '/static/'
+    MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+    MEDIA_URL = '/media/'
+else:
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+    MEDIA_ROOT = os.path.join(REMOTE_ROOT, 'media_' + APP_ENV)
+    STATIC_URL = 'http://' + APP_ENV + '.static.ohrockman.com/'
+    MEDIA_URL = 'http://' + APP_ENV + '.media.ohrockman.com/'
+
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_DIR, 'static'),
 )
-
-from photologue import PHOTOLOGUE_APP_DIR
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(PROJECT_DIR, 'templates'),
     PHOTOLOGUE_APP_DIR,
 )
+
+RAVEN_CONFIG = {
+    'dsn': 'http://4c42444377c04669b7d5abd62405e99a:2fc1ca8df97d4fd6928e34b5f148051c@sentry.rocktavious.com/3',
+}
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 try:
     from ohrockman.local_settings import *
